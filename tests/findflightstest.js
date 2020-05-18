c4fa721079609819ccaef604e2bb9aa3a33da953
avi <avi.vishwakumar@aesop.com>
2020-05-18 14:47:53 +1000

    request logger hook for test

--- tests/findflightstest.js
+++ tests/findflightstest.js
@@ -5,12 +5,16 @@ import minimist from 'minimist';
 import Home from '../objects/Home'
 import Results from '../objects/flightresults'
 
+//Request logger config
+import { RequestLogger } from 'testcafe'
+
 //Load libraries to load and read config files
 const fs = require('fs');
 const yaml = require('js-yaml');
-
 const home = new Home();
 const results = new Results();
+
+
 
 //extracts values from the command line to then retrieve the relevant config files
 //const args = minimist(process.argv.slice(2));
@@ -19,6 +23,7 @@ const results = new Results();
 
 //Load the config relevant to the supplied environment variable.
 const config = yaml.safeLoad(fs.readFileSync("config.yml","utf8"));
+const logger = RequestLogger(`${config.url}`);
 const data =require("../data.json")
 //__dirname + "/config.yml", "utf8"));
 
@@ -26,6 +31,7 @@ const data =require("../data.json")
 fixture`Blaze Demo`
 //${env} ${locale}
     .page `${config.url}`
+    .requestHooks(logger);
 
 
 test('Find flights', async t => {
@@ -34,5 +40,6 @@ test('Find flights', async t => {
     await home.findflights(t, config.from, config.to)
     //Validate the search results
     await results.validateSearchResults(t, data);
+    console.log(logger.requests);
     //await login.loginUser(t, config.webusername, config.webpassword);
 })
